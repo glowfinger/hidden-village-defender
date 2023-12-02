d95631542882705ca8dfd5b3ed393808dd32bf9b
Darren Lilley <darren@glowfinger.com>
2023-12-02 09:41:02 +0000

    main screen

--- src/lib/classes/game/GameSceneConfig.ts
+++ src/lib/classes/game/GameSceneConfig.ts
@@ -4,10 +4,11 @@ import SplashScene from '../scenes/SplashScene';
 import StartScene from '../scenes/StartScene';
 
 export default function gameSceneConfig(game: Phaser.Game) {
-  game.scene.add('SplashScreen', SplashScene);
+  game.scene.add('SplashScene', SplashScene);
   game.scene.add('StartScene', StartScene);
   game.scene.add('GameScene', GameScene);
   game.scene.add('GameUIScene', GameUIScene);
-  game.scene.start('GameScene');
-  game.scene.start('GameUIScene');
+  game.scene.start('SplashScene');
+  // game.scene.start('GameScene');
+  // game.scene.start('GameUIScene');
 }
